b413a354da4d4df1355b1828ccbd8ecbe6a54375
SriSaikiran K T <srisaikirantambalkar@gmail.com>
2026-08-05 08:47:34 +0530

    Time: 1 ms (97.62%), Space: 42.5 MB (79.52%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,6 +1,17 @@
 class Solution {
     public int mySqrt(int x) {
-        int n= (int)Math.floor(Math.sqrt(x));
-        return n;
+        if(x==0 || x==1) return x;
+        int l=1; int r=x;
+        int ans=0;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(mid<=x/mid){
+                ans=mid;
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+        }
+        return ans;
     }
 }
