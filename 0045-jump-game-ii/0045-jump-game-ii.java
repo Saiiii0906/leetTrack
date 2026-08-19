6130ea3d2492bdb300beeb5c03db31b74f75e50e
SriSaikiran K T <srisaikirantambalkar@gmail.com>
2026-08-19 23:26:22 +0530

    Time: 1 ms (99.76%), Space: 47.2 MB (76.86%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n=nums.length;
+        int jumps=0, end=0, far=0;
+        for(int i=0; i<n-1; i++){
+            far=Math.max(far, i+nums[i]);
+            if(i==end){
+                jumps++;
+                end = far;
+            }
+        }
+        return jumps;
+    }
+}
